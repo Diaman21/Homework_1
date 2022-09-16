d8858f18030e37d35d18c50f847f466b86131de9
Diaman21 <diaman21@mail.ru>
2022-09-16 16:41:03 +0300

    Задание 4

---
+++ №4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<0)
+{
+    Console.Write("Введите положительное число");
+}
+else{
+    while (num>1)
+    {
+        if ((num % 2) == 0)
+        {
+            Console.Write(num + ";");
+        }
+        num = num - 1;
+    }
+}
